e03e85303d009a8cc2f3a452d02fe01c3b2382b5
Gerrit Dongus <gdongus@gmail.com>
2015-12-05 05:00:56 +0100

    Update TestClass.java

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -1,12 +1,5 @@
 package Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: gd
- * Date: 12/5/15
- * Time: 2:18 AM
- * To change this template use File | Settings | File Templates.
- */
 public class TestClass {
     public static void foo() {
         System.out.println("Hello World \n");
