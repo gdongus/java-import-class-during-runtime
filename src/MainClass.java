84f93066f2f13b7c75c81881954a84dcc4205771
gerrit.dongus <gerrit.dongus@mindsmash.com>
2015-12-05 05:32:40 +0100

    beautify

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,44 +5,70 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainClass {
-    public static void main(String[] args) throws URISyntaxException, MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
 
-        System.out.println("Load jar file: " + args[0]);
+    private final static Logger LOG = Logger.getLogger(MainClass.class.getName());
+
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+        LOG.setLevel(Level.INFO);
+        LOG.info("Load jar file: " + args[0]);
         URL resource = Thread.currentThread().getContextClassLoader().getResource(args[0]);
 
-        System.out.println("Get URLClassLoader: " + args[0]);
-        URLClassLoader sysLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();
-        Class sysClass = URLClassLoader.class;
-        Method sysMethod = sysClass.getDeclaredMethod("addURL",new Class[] {URL.class});
-        sysMethod.setAccessible(true);
-
-        System.out.println("Add jar file to ClassLoader: "+ args[0]);
-        sysMethod.invoke(sysLoader, new Object[]{resource.toURI().toURL()});
-
-        System.out.println("Loading class: " + args[1]);
-        Class MyClass = sysLoader.loadClass(args[1]);
-
-        System.out.println("Found class methods: ");
-        for (Method method : MyClass.getDeclaredMethods()) {
-            final StringBuffer sb = new StringBuffer();
-            sb.append(MyClass.getName() + ".");
-            sb.append(method.getName()+ "( ");
-            for (Type type : method.getParameterTypes()) {
-                sb.append(" " + type.toString());
-            }
-            sb.append(" )");
-            System.out.println(sb.toString());
+        if (resource == null) {
+            LOG.severe("Failed to load jar file : " + args[0]);
+            System.exit(1);
         }
 
-        System.out.println("Instantiating method: " + args[2]);
-        Method printMeMethod = MyClass.getMethod(args[2], new Class[]{});
+        LOG.info("Get URLClassLoader: " + args[0]);
+        URLClassLoader sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+        Class sysClass = URLClassLoader.class;
+        Method sysMethod = sysClass.getDeclaredMethod("addURL", new Class[]{URL.class});
+        sysMethod.setAccessible(true);
 
-        System.out.println("Creating new instance of class: " + args[1]);
-        Object MyClassObj = MyClass.newInstance();
+        LOG.info("Add jar file to ClassLoader: " + args[0]);
+        sysMethod.invoke(sysLoader, new Object[]{resource.toURI().toURL()});
 
-        System.out.println("Executing method: " + args[2]);
-        printMeMethod.invoke(MyClassObj);
+        try {
+            LOG.info("Loading class: " + args[1]);
+            final Class clazz = sysLoader.loadClass(args[1]);
+
+            LOG.info("Found class methods: ");
+            for (Method method : clazz.getDeclaredMethods()) {
+                final StringBuffer sb = new StringBuffer();
+                sb.append(clazz.getName() + ".");
+                sb.append(method.getName() + "( ");
+                for (Type type : method.getParameterTypes()) {
+                    sb.append(" " + type.toString());
+                }
+                sb.append(" )");
+                LOG.info(sb.toString());
+            }
+
+            try {
+                LOG.info("Creating new instance of class: " + args[1]);
+                Object MyClassObj = clazz.newInstance();
+
+                LOG.info("Instantiating method: " + args[2]);
+                Method printMeMethod;
+                try {
+                    printMeMethod = clazz.getMethod(args[2], new Class[]{});
+                    LOG.info("Executing method: " + args[2]);
+                    printMeMethod.invoke(MyClassObj);
+
+                } catch (Exception ex) {
+                    LOG.severe("Failed to instantiate method: " + args[2]);
+                    System.exit(1);
+                }
+            } catch (Exception ex) {
+                LOG.severe("Failed to instantiate class: " + args[1]);
+                System.exit(1);
+            }
+        } catch (Exception ex) {
+            LOG.severe("Failed to load class: " + args[1]);
+            System.exit(1);
+        }
     }
 }
